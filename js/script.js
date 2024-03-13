23d37169d3d743d2a9447ea9dfed69e6bd91b098
unium <81354905+TheUnium@users.noreply.github.com>
2024-03-13 15:05:49 +0530

    omg draggable windows : part 2

--- js/script.js
+++ js/script.js
@@ -11,6 +11,12 @@ document.getElementById("current_year").innerHTML = new Date().getFullYear();
 setInterval(updateTime, 1000);
 updateTime();
 
+dragElement(document.getElementById("win1"));
+dragElement(document.getElementById("win2"));
+dragElement(document.getElementById("win3"));
+dragElement(document.getElementById("win4"));
+function dragElement(e){var n=0,t=0,o=0,l=0;function u(u){(u=u||window.event).preventDefault(),n=o-u.clientX,t=l-u.clientY,o=u.clientX,l=u.clientY,e.style.top=e.offsetTop-t+"px",e.style.left=e.offsetLeft-n+"px"}function f(){document.onmouseup=null,document.onmousemove=null}e.onmousedown=function e(n){(n=n||window.event).preventDefault(),o=n.clientX,l=n.clientY,document.onmouseup=f,document.onmousemove=u}}
+
 // from : https://znci.dev/js/index.js
 const isMobile = window.matchMedia("(max-width: 862px)").matches;
 const acknowledgedMobileAlert =
